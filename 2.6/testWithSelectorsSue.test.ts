5b2fe56d805aea29cda9790905e4968e747d6703
Susan Gross <susanlynngross@gmail.com>
2022-11-26 18:41:38 -0500

    HW 2.6

--- 2.6/testWithSelectorsSue.test.ts
+++ 2.6/testWithSelectorsSue.test.ts
@@ -12,13 +12,13 @@ describe("Filling in the blanks", () => {
         await driver.quit();
     })
 
-    const hdrInput: By = By. //fill in the blank
-    const mkeInput: By = By. //fill in the blank
-    const oaiInput: By = By. //fill in the blank
-    const nameInput: By = By. //fill in the blank
-    const clrBtn: By = By. //fill in blank 
-    const submitBtn: By = By. //fill in blank
-    const errorMsg: By = By. // fill in blank 
+    const hdrInput: By = By.name('hdrInput');
+    const mkeInput: By = By.name('mkeInput');
+    const oaiInput: By = By.name('oriInput');
+    const nameInput: By = By.name('namInput');
+    const clrBtn: By = By.id('clearBtn');
+    const submitBtn: By = By.id('saveBtn');
+    const errorMsg: By = By.id('validHeader');
 
     test("filling in the blanks for real", () => {
         await driver.findElement(hdrInput).sendKeys("Change this")
